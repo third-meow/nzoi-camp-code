6af81be5440b1033a6a5fde68cc181213e36c496
The third meow <jsheaandcats@gmail.com>
2019-07-01 10:08:19 +1200

    attempted students(dp)

--- dp/students.cpp
+++ dp/students.cpp
@@ -1,10 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<uint32_t> dp_tab = {1};
 
+uint32_t fact(int n, uint32_t mod) {
+	uint32_t run = 1;
+	for(int i = dp_tab.size(); i <= n; ++i) {
+		run = (run * i) % mod;
+	}
+	return run;
+}
 
 int main() {
+	int student_n, required;
 	cin >> student_n >> required;
+
+	unsigned long long 
+	cout << ( fact(student_n) / ( fact(student_n - required) * fact(required) ) ) % 100000 << endl;
+
+	/*
+	for (int i = 0; i < 20; ++i) {
+		cout << fact_ro(i) << endl;
+		cout << fact_no(i) % 100000 << endl;
+		cout << fact(i) << endl;
+		cout << endl << endl;
+	}
+	*/
 
 
 
