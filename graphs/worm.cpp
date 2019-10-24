0569b68757c5b31e6f27c8f0661f9e1e30525351
The third meow <jsheaandcats@gmail.com>
2019-10-24 20:45:53 +1300

    redo worm, still not working

--- graphs/worm.cpp
+++ graphs/worm.cpp
@@ -2,10 +2,27 @@
 using namespace std;
 #define INF 2000000
 
+vector<vector<vector<int>>> adj_list;
+
+bool solve(int pos, vector<pair<bool, int>> been, int time) {
+	been[pos] = make_pair(true, time);
+	for (vector<int> neighbour : adj_list[pos]) {
+		if (been[neighbour[0]].first) {
+			if ((time - been[neighbour[0]].second) < 0) {
+				return true;
+			}
+		}
+		if (solve(neighbour[0], been, time+neighbour[1])) {
+			return true;
+		}
+	}
+	return false;
+}
+
 int main(int argc, char *argv[]) {
 	int  node_n, edge_n;
 	cin >> node_n >> edge_n;
-	vector<vector<vector<int>>> adj_list(node_n, vector<vector<int>>());
+	adj_list = vector<vector<vector<int>>>(node_n, vector<vector<int>>());
 
 	{
 		int from, to, time_dif;
@@ -15,10 +32,22 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	vector<bool> been(node_n, false);
-	vector<int> beentimes(node_n, 0);
+	vector<pair<bool, int>> been(node_n, make_pair(false, 0));
 	int n = 0;
 	int time = 0;
+
+	if (solve(0, been, 0)) {
+		cout << "posssible" << endl;
+	} else {
+		cout << "not possible" << endl;
+	}
+
+
+
+	return 0;
+}
+
+/*
 	while (true) {
 
 		if (been[n]) {
@@ -40,9 +69,9 @@ int main(int argc, char *argv[]) {
 			return 0;
 		}
 
-		/*
+		 *
 		 * select best neighbour (TODO: should probably try all of them tho)
-		 */
+		 *
 		vector<int> best_neighbour = {0, INF};
 		for(auto neighbour : neighbours) {
 			if (neighbour[1] < best_neighbour[1]) {
@@ -53,9 +82,4 @@ int main(int argc, char *argv[]) {
 		n = best_neighbour[0];
 		time += best_neighbour[1];
 	}
-		
-	
-
-	return 0;
-}
-
+	*/
