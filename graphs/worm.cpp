113a78b55e127fd62db646770e7a9bd3a48b3ada
The third meow <jsheaandcats@gmail.com>
2019-11-03 18:43:24 +1300

    change approach for worm

--- graphs/worm.cpp
+++ graphs/worm.cpp
@@ -2,55 +2,73 @@
 using namespace std;
 #define INF 2000000
 
+
 vector<vector<vector<int>>> adj_list;
 
-bool solve(int pos, vector<pair<bool, int>> been, int time) {
-	been[pos] = make_pair(true, time);
-	for (vector<int> neighbour : adj_list[pos]) {
-		if (been[neighbour[0]].first) {
-			if ((time + neighbour[1] - been[neighbour[0]].second) < 0) {
-				/*
-				cout << "time - been(neighbour) is smaller than 0" << endl;
-				cout << "pos: " << pos << endl;
-				cout << "neighbour: " << neighbour[0] << endl;
-				cout << "been[neighbour]: " << been[neighbour[0]].second << endl;
-				cout << "time: " << time << endl;
-				*/
-				return true;
-			}
-		} else if (solve(neighbour[0], been, time+neighbour[1])) {
-			return true;
-		}
+int cycle_check(int start, int node_n) {
+	int best = 1;
+	//cout << adj_list[2].size() << endl;
+	vector<int> been(node_n,	INF);
+	queue<vector<int>> q;
+	for(vector<int> neighbour : adj_list[start]) {
+		q.push({neighbour[0], neighbour[1]});
 	}
-	return false;
+	while(q.empty() == false) {
+		cout << "yeet" << endl;
+		int node = q.front()[0];
+		int time = q.front()[1];
+		q.pop();
+		if (been[node] < time) continue;
+		been[node] = time;
+		//cout << node << "\t" << time << "\t" << adj_list[node].size() << endl;
+		if (node == start) {
+			best = min(time, best);
+			if (best < 0) return best;
+		}
+		for(vector<int> neighbour : adj_list[node]) {
+			q.push({neighbour[0], neighbour[1]+time});
+		}
+		//cout << endl;
+	}
+	return best;
+}
+
+int take_case() {
+	int  node_n, edge_n;
+	cin >> node_n >> edge_n;
+	adj_list = vector<vector<vector<int>>>(node_n, vector<vector<int>>());
+
+	int from, to, time_dif;
+	for(int i = 0; i < edge_n; ++i) {
+		cin >> from >> to >> time_dif;
+		adj_list[from].push_back({to, time_dif});
+	}
+
+	vector<bool> been(node_n, false);
+	queue<int> q;
+	q.push(0);
+	while(q.empty() == false) {
+		int node = q.front();
+		q.pop();
+		if (been[node]) continue;
+		been[node] = true;
+		if (cycle_check(node, node_n) < 0) {
+			cout << "possible" << endl;
+			return 0;
+		}
+		for(vector<int> neighbour : adj_list[node]) {
+			q.push(neighbour[0]);
+		} 
+	} 
+	cout << "not possible" << endl; 
+	return 0;
 }
 
 int main(int argc, char *argv[]) {
 	int testcase_n;
 	cin >> testcase_n;
 	for(int j = 0; j < testcase_n; ++j) {
-		int  node_n, edge_n;
-		cin >> node_n >> edge_n;
-		adj_list = vector<vector<vector<int>>>(node_n, vector<vector<int>>());
-
-		{
-			int from, to, time_dif;
-			for(int i = 0; i < edge_n; ++i) {
-				cin >> from >> to >> time_dif;
-				adj_list[from].push_back({to, time_dif});
-			}
-		}
-
-		vector<pair<bool, int>> been(node_n, make_pair(false, 0));
-		int n = 0;
-		int time = 0;
-
-		if (solve(0, been, 0)) {
-			cout << "posssible" << endl;
-		} else {
-			cout << "not possible" << endl;
-		}
+		take_case();
 	}
-
 	return 0;
 }
