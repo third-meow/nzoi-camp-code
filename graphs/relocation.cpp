b93401143090a39aa896172add948060ae51e744
The third meow <jsheaandcats@gmail.com>
2019-11-03 21:46:16 +1300

    working towards building min spanning tree of market towns

--- graphs/relocation.cpp
+++ graphs/relocation.cpp
@@ -2,10 +2,25 @@
 using namespace std;
 #define INF 2000000
 
+bool cycle_check(const vector<vector<vector<int>>>& adj_list, int pos, int root, vector<bool> visited) {
+	visited[pos] = true;
+	for(vector<int> neighbour : adj_list[pos]) {
+		if (neighbour[0] == root) continue;
+		if (visited[neighbour[0]]) {
+			return true;
+		}
+		if (cycle_check(adj_list, neighbour[0], pos, visited)) {
+			return true;
+		}
+	}
+	return false;
+}
+
 int main(int argc, char *argv[]) {
 	int node_n, edge_n, market_n;
 	cin >> node_n >> edge_n >> market_n;
 
+	vector<vector<int>> edge_list(edge_n, vector<int>());
 	vector<vector<vector<int>>> adj_list(node_n, vector<vector<int>>());
 	vector<int> markets(market_n, 0);
 	vector<bool> is_town(node_n, true);
@@ -22,6 +37,7 @@ int main(int argc, char *argv[]) {
 		--a;--b;
 		adj_list[a].push_back({b, c});
 		adj_list[b].push_back({a, c});
+		edge_list.push_back({c, a, b});
 	}
 
 	int closest_town = INF;
@@ -34,12 +50,14 @@ int main(int argc, char *argv[]) {
 	}
 
 	cout << closest_town << endl;
-
-
-
-
-
 	
+	//cycle_check(adj_list, 0, 0, vector<bool>(node_n, false))
+	/* 
+	 * sort edge list by weight
+	 * for edge in edge list, add to second adj list, check second adj list for cycles
+	 *  true - somehow remove??
+	 *  false - continue
+	 */
 
 	return 0;
 }
