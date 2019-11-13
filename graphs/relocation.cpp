2a447b4d35468e2e38208475fe5b33239b2036bd
The third meow <jsheaandcats@gmail.com>
2019-11-13 22:09:05 +1300

    rewrite relocation for traveling salesman approch

--- graphs/relocation.cpp
+++ graphs/relocation.cpp
@@ -1,167 +1,74 @@
 #include<bits/stdc++.h>
-using namespace std;
-#define INF 2000000
+using namespace std; 
+#define INF 2000000 
+vector<vector<vector<int>>> adj_list;
+vector<vector<vector<int>>> targets_left;
 
-bool cycle_check(const vector<vector<vector<int>>>& adj_list, int pos, int root, vector<bool> visited) {
-	visited[pos] = true;
-	for(vector<int> neighbour : adj_list[pos]) {
-		if (neighbour[0] == root) continue;
-		if (visited[neighbour[0]]) {
-			return true;
-		}
-		if (cycle_check(adj_list, neighbour[0], pos, visited)) {
-			return true;
-		}
+string pp(vector<int>& list) {
+	stringstream ss;
+	for(auto i : list) {
+		ss << i << " ";
 	}
-	return false;
+	return ss.str();
+}
+
+
+int ts(int pos, vector<int> targets, int goal) {
+	// if weve reached the goal and have 0 targets, return 0
+	if (pos == goal && targets.size() == 0) {
+		cout << 0 << endl;
+		return 0;
+	}
+	
+	// if weve been here before, and had less targets
+	if (find(targets_left[pos].begin(), targets_left[pos].end(),targets) != targets_left[pos].end()) 
+		return INF;
+
+	targets_left[pos].push_back(targets);
+	// data dump
+	//cout << endl << endl;
+	//cout << "pos: " << pos << endl;
+	//cout << "targets: " << pp(targets) << endl;
+	//cout << "targets left : " << pp(targets_left[pos]) << endl;
+
+
+	// if weve reached a target, erase it from vector
+	auto it = find(targets.begin(), targets.end(), pos);
+	if (it != targets.end()) targets.erase(it);
+
+	int c_min = INF;
+	for(vector<int> neighbour : adj_list[pos]) {
+		c_min = min(ts(neighbour[0], targets, goal) + neighbour[1], c_min);
+	}
+	cout << c_min << endl;
+	return c_min;
 }
 
 int main(int argc, char *argv[]) {
-	int node_n, edge_n, market_n;
+	int market_n, edge_n, node_n;
 	cin >> node_n >> edge_n >> market_n;
 
-	vector<vector<int>> edge_list(edge_n, vector<int>());
-	vector<vector<vector<int>>> adj_list(node_n, vector<vector<int>>());
 	vector<int> markets(market_n, 0);
-	vector<bool> is_town(node_n, true);
-
-	for (int i = 0; i < market_n; ++i) {
+	for (int i = 0; i < market_n; ++i){
 		cin >> markets[i];
-		--markets[i];
-		is_town[markets[i]] = false;
+		markets[i]--;
 	}
 
+	adj_list.assign(node_n, vector<vector<int>>());
+	targets_left.assign(node_n, vector<vector<int>>());
 	int a, b, c;
-	for (int i = 0; i < edge_n; ++i) {
+	for(int i = 0; i < edge_n; ++i) {
 		cin >> a >> b >> c;
-		--a;--b;
+		c--;
+		b--;
+		a--;
 		adj_list[a].push_back({b, c});
 		adj_list[b].push_back({a, c});
-		edge_list[i] = {c, a, b};
 	}
 
-	int closest_town = INF;
-	for (int market : markets) {
-		for (vector<int> neighbour : adj_list[market]) {
-			if (is_town[neighbour[0]]) {
-				closest_town = min(closest_town, neighbour[1]);
-			}
-		}
-	}
-
+	cout << ts(4, {0, 1, 2}, 4) << endl;
 	
-	/* 
-	 * sort edge list by weight
-	 * for edge in edge list, add to second adj list, check second adj list for cycles
-	 *  true - somehow remove??
-	 *  false - continue
-	 */
-
-	// remove non market-market edges
-	bool done_flag;
-	do {
-		done_flag = true;
-		for (int i = 0; i < edge_list.size(); ++i) {
-			if (is_town[edge_list[i][1]] || is_town[edge_list[i][2]]) {
-				edge_list.erase(edge_list.begin()+i);
-				done_flag = false;
-			}
-		}
-	} while (done_flag == false);
-
-
-	// sort edge list in aceding order of weight
-	sort(edge_list.begin(), edge_list.end(), [](vector<int> a, vector<int> b) {
-			return a[0] < b[0];
-	});
-
-	int min_span_tree = 0;
-	vector<vector<vector<int>>> min_span_adj_list(node_n, vector<vector<int>>());
-	for(auto edge : edge_list) {
-		min_span_adj_list[edge[1]].push_back({edge[2], edge[0]});
-		min_span_adj_list[edge[2]].push_back({edge[1], edge[0]});
-		if (cycle_check(min_span_adj_list, edge[1], edge[1], vector<bool>(node_n, false))) {
-			min_span_adj_list[edge[1]].pop_back();
-			min_span_adj_list[edge[2]].pop_back();
-		} else {
-			min_span_tree += edge[0];
-		}
-	}
-
-
-
-	cout << (closest_town + min_span_tree) * 2 << endl;
-
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
