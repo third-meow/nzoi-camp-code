954fa92d886d5ece1b9104b78fb222ebe9d0c987
The third meow <jsheaandcats@gmail.com>
2019-11-14 21:46:00 +1300

    i thought we'd done it

--- graphs/relocation.cpp
+++ graphs/relocation.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std; 
 #define INF 2000000 
+
+int market_n, edge_n, node_n;
 vector<vector<vector<int>>> adj_list;
 vector<vector<vector<int>>> targets_left;
 
@@ -12,46 +14,50 @@ string pp(vector<int>& list) {
 	return ss.str();
 }
 
+int dika(int from, int to) {
+	priority_queue<pair<int, int>, vector<pair<int, int>>, std::greater<pair<int, int>>> pq;
+	vector<int> been(node_n, INF);
+
+	pq.push(make_pair(from, 0));
+	while(pq.empty() == false) {
+		pair<int, int> n = pq.top(); pq.pop();
+
+		if (been[n.first] <= n.second) continue;
+		been[n.first] = n.second;
+
+		if (n.first == to) return n.second;
+
+		for(auto neighbour : adj_list[n.first]) {
+			pq.push(make_pair(neighbour[0], neighbour[1]+n.second));
+		}
+	} return INF;
+}
+
 
 int ts(int pos, vector<int> targets, int goal) {
-	// if weve reached the goal and have 0 targets, return 0
-	if (pos == goal && targets.size() == 0) {
-		cout << 0 << endl;
-		return 0;
-	}
-	
-	// if weve been here before, and had less targets
-	if (find(targets_left[pos].begin(), targets_left[pos].end(),targets) != targets_left[pos].end()) 
-		return INF;
-
-	targets_left[pos].push_back(targets);
-	// data dump
-	//cout << endl << endl;
-	//cout << "pos: " << pos << endl;
-	//cout << "targets: " << pp(targets) << endl;
-	//cout << "targets left : " << pp(targets_left[pos]) << endl;
-
-
-	// if weve reached a target, erase it from vector
-	auto it = find(targets.begin(), targets.end(), pos);
-	if (it != targets.end()) targets.erase(it);
-
-	int c_min = INF;
-	for(vector<int> neighbour : adj_list[pos]) {
-		c_min = min(ts(neighbour[0], targets, goal) + neighbour[1], c_min);
-	}
-	cout << c_min << endl;
-	return c_min;
+	sort(targets.begin(), targets.end());
+	int score;
+	int min_score = INF;
+	do {
+		score = dika(pos, targets[0]);
+		for(int i = 1; i < targets.size(); ++i) {
+			score += dika(targets[i-1], targets[i]);
+		}
+		score += dika(targets[targets.size()-1], goal);
+		min_score = min(score, min_score);
+	} while(next_permutation(targets.begin(), targets.end()));
+	return min_score;
 }
 
 int main(int argc, char *argv[]) {
-	int market_n, edge_n, node_n;
 	cin >> node_n >> edge_n >> market_n;
+	vector<int> istown(node_n, true);
 
 	vector<int> markets(market_n, 0);
 	for (int i = 0; i < market_n; ++i){
 		cin >> markets[i];
 		markets[i]--;
+		istown[markets[i]] = false;
 	}
 
 	adj_list.assign(node_n, vector<vector<int>>());
@@ -59,14 +65,30 @@ int main(int argc, char *argv[]) {
 	int a, b, c;
 	for(int i = 0; i < edge_n; ++i) {
 		cin >> a >> b >> c;
-		c--;
 		b--;
 		a--;
 		adj_list[a].push_back({b, c});
 		adj_list[b].push_back({a, c});
 	}
 
-	cout << ts(4, {0, 1, 2}, 4) << endl;
+	/*
+	cout << endl << endl;
+	for (int i = 0; i < node_n; ++i) {
+		auto r = adj_list[i];
+		cout << i << ": ";
+		for (auto e : r) {
+			cout << e[0] << "-" << e[1] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl << endl; */
+
+	int c_min = INF;
+	for(int i = 0; i < node_n; ++i) {
+		if (istown[i])
+			c_min = min(c_min, ts(i, markets, i));
+	}
+	cout << c_min << endl;
 	
 
 	return 0;
